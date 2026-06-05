e9f017aeaef6a4847005f5ab244ce8dc44a457cd
Vijayakash-588 <akashwolf0508@gmail.com>
2026-06-05 16:54:33 +0530

    resume updated

--- src/components/SocialIcons.tsx
+++ src/components/SocialIcons.tsx
@@ -80,7 +80,7 @@ const SocialIcons = () => {
           </a>
         </span>
       </div>
-      <a className="resume-button" href="#">
+      <a className="resume-button" href="/images/L.V.Reshma shree.pdf" target="_blank" rel="noopener noreferrer">
         <HoverLinks text="RESUME" />
         <span>
           <TbNotes />
