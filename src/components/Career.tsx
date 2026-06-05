06e66626b14a1f98bdcc8543933ef7ef8746b019
Vijayakash-588 <akashwolf0508@gmail.com>
2026-06-05 11:04:47 +0530

    intern updated and images mockup added

--- src/components/Career.tsx
+++ src/components/Career.tsx
@@ -39,13 +39,13 @@ const Career = () => {
               insights to support UX decision-making and feature prioritization.
             </p>
           </div>
-                    <div className="career-info-box">
+          <div className="career-info-box">
             <div className="career-info-in">
               <div className="career-role">
-                <h4>Digital Marketing intern || Graphics Design</h4>
+                <h4>Graphics Design Intern</h4>
                 <h5>DVILITE</h5>
               </div>
-              <h3>FEB–MAY 2026</h3>
+              <h3>FEB–JUN 2026</h3>
             </div>
             <p>
               Developed responsive,user-friendly carousels and promotion designs.Applied SEO fundamentals and modern UI/UX practices to improve user engagement.Helped digital marketing team to improve company growth and promotions.
